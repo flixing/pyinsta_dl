fceddec3e2cf59b965a763a1ad203c34449df6f1
natanocr <natanoliveiracruz@gmail.com>
2016-10-27 09:00:00 -0300

    modification in the comments

--- pyinsta_dl/__init__.py
+++ pyinsta_dl/__init__.py
@@ -2,8 +2,8 @@
 Sample Usage:
 
 >>> import pyinsta_dl
->>> url = pyinsta_dl.dowload('https://www.instagram.com/p/BL_wcRcjS_C/')
->>> print url
+>>> url = pyinsta_dl.get('https://www.instagram.com/p/BL_wcRcjS_C/')
+>>> print(url)
 https://instagram.fgru3-2.fna.fbcdn.net/t51.2885-15/e35/14719344_188429321607052_4168051712482869248_n.jpg?ig_cache_key=MTM2OTAyNTg2MDg1NDQyNzU4Ng%3D%3D.2
 
 """
@@ -25,7 +25,7 @@ class PyInsta_DL(object):
                 return meta.get("content")
         return None
 
-DEFAULT_DOWNLOAD =  PyInsta_DL()
+DEFAULT_DOWNLOAD = PyInsta_DL()
 
 
 def get(url):
