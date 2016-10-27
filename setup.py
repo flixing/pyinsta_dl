9bb270259c353e7f113857358132f65a3bd631d0
natanocr <natanoliveiracruz@gmail.com>
2016-10-27 11:22:35 -0300

    README UPDATE

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
-import io
-import os
-from setuptools import setup
-
-
-def read(fname):
-    file_path = os.path.join(os.path.dirname(__file__), fname)
-    return io.open(file_path, encoding='utf-8').read()
 
 setup(name='pyinsta_dl',
       version='0.0.1',
@@ -16,10 +8,10 @@ setup(name='pyinsta_dl',
       author='Natan Oliveira',
       author_email='natanoliveiracruz@gmail.com',
       keywords='instagram download media image video',
-      long_description=read('README.rst'),
+      long_description=open('README.rst').read(),
       license='MIT',
       packages=['pyinsta_dl'],
       install_requires=[
-            'bs4',
+          'bs4',
       ],
       zip_safe=False)
