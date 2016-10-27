a7cd2f04084475c6bdbbf5c69018ec8f38f8df6b
natanocr <natanoliveiracruz@gmail.com>
2016-10-27 16:56:05 -0300

    implementation of tests

--- pyinsta_dl/__init__.py
+++ pyinsta_dl/__init__.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Sample Usage:
-
->>> import pyinsta_dl
->>> url = pyinsta_dl.get('https://www.instagram.com/p/BL_wcRcjS_C/')
->>> print(url)
-https://instagram.fgru3-2.fna.fbcdn.net/t51.2885-15/e35/14719344_188429321607052_4168051712482869248_n.jpg?ig_cache_key=MTM2OTAyNTg2MDg1NDQyNzU4Ng%3D%3D.2
-
-"""
-
 import urllib2
 from bs4 import BeautifulSoup
-
 
 class PyInsta_DL(object):
 
@@ -26,7 +15,6 @@ class PyInsta_DL(object):
         return None
 
 DEFAULT_DOWNLOAD = PyInsta_DL()
-
 
 def get(url):
     return DEFAULT_DOWNLOAD.get(url)
