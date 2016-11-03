3d9855d8006542b4a68d9e52d692b58e54812395
natanocr <natanoliveiracruz@gmail.com>
2016-11-03 12:21:43 -0300

    Revert "remove try"
    This reverts commit 29990a1694b76d2a7186f7ae5c6eeaf4d41c3d7c.

--- pyinsta_dl/__init__.py
+++ pyinsta_dl/__init__.py
@@ -20,12 +20,16 @@ class PyInsta_DL(object):
 
     def get(self, url):
         self.url = url
-        soup = BeautifulSoup(urllib2.urlopen(self.url).read(), "html.parser")
+        try:
+            soup = BeautifulSoup(urllib2.urlopen(self.url).read(), "html.parser")
+        except ValueError:
+            pass
         for meta in soup.findAll('meta'):
             if meta.get("property") == "og:image" and meta.get("content") != '':
                 return meta.get("content")
             elif meta.get("property") == "og:video" and meta.get("content") != '':
                 return meta.get("content")
+        return None
 
     def get_all(self, url):
         self.url = url
