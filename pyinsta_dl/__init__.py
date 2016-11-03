83259846cde5186865832b5089997d6fe47c33c6
natanocr <natanoliveiracruz@gmail.com>
2016-11-03 11:14:25 -0300

    performance enhancements

--- pyinsta_dl/__init__.py
+++ pyinsta_dl/__init__.py
@@ -12,8 +12,7 @@ class PyInsta_DL(object):
 
         self.userAgent = 'Mozilla/5.0 (Linux; U; Android 2.2.1; en-ca; LG-P505R Build/FRG83) \
         AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
-        self.phantomjs ='/usr/local/phantomjs/phantomjs'
-        #os.chmod(self.phantomjs, stat.S_IEXEC)
+        self.phantomjs = '/usr/local/phantomjs/phantomjs'
         DesiredCapabilities.PHANTOMJS[
             'phantomjs.page.customHeaders.User-Agent'] = self.userAgent
         self.driver = webdriver.PhantomJS(executable_path=self.phantomjs, \
@@ -23,8 +22,8 @@ class PyInsta_DL(object):
         self.url = url
         try:
             soup = BeautifulSoup(urllib2.urlopen(self.url).read(), "html.parser")
-        except:
-            return NotImplementedError
+        except ValueError:
+            pass
 
         for meta in soup.findAll('meta'):
             if meta.get("property") == "og:image" and meta.get("content") != '':
@@ -37,8 +36,7 @@ class PyInsta_DL(object):
         self.url = url
         self.driver.get(self.url)
         time.sleep(5)
-        for i in range(self.count_scrolls()):
-            self.scroll()
+        [self.scroll() for i in range(self.count_scrolls()) ]
         return self._get_all_urls
 
     def count_scrolls(self):
@@ -62,8 +60,7 @@ class PyInsta_DL(object):
         images = self.driver.find_elements_by_class_name('_icyx7')
         links = []
         for i in images:
-            if i != None:
-                links.append(i.get_attribute('src'))
+            links.append(i.get_attribute('src'))
         return links
 
 
